f9885ae9d318bbd8700e73613c124d4ee635de71
KJCSajonia <125939107+KJCSajonia@users.noreply.github.com>
2025-06-18 09:51:03 +0800

    First Commit
    Semi-done with front-end

---
+++ resume/app/Views/templates/header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <meta name="description" content="Resume of Kezia Sajonia">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=call" /
+    >
+    
+    <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+          
+    <style>
+        body {background-color: #FFF0F0;}
+        .title {
+            color: black;
+            font-size: 4rem;
+        
+        }
+        .contents {
+            
+        }
+
+        .contents li {
+        display: inline-block;
+     
+        }
+        .label {
+            margin-left: 2%;
+            border: 3px solid;     
+        }
+        .content{
+
+            margin-bottom: 10%;
+            border: 2px solid;
+        }
+
+
+
+
+    </style>
+</head>
+<body>
+    <h1 class = "title"><?= esc($title) ?></h1>
---
+++ resume/app/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sajonia Resume</title>
+</head>
+<body>
+
+<h1>Kezia Sajonia</h1>
+
+
+</body>
+</html>
